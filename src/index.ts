5dd4a91edce8a399c5837db2751202e639e429a9
Vladislav Bulyukhin <VladislavB@kentico.com>
2020-09-01 11:32:20 +0200

    KCL-5330 Remove component helper for now

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
 export { default } from './plugin';
 export { buildKontentLink, buildElementLink, buildComponentElementLink } from './utils/link';
-export { isContentComponent } from './utils/contentComponent';
