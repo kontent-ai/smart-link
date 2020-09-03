14c5d7ef3b2c9ad3f76198feba74273bdc3f295f
Vladislav Bulyukhin <VladislavB@kentico.com>
2020-09-03 17:12:39 +0200

    KCL-4843 Prepare unit tests for SDK - Setup Karma and plugins - Prepate unit tests for utils

---
+++ test-browser/utils/storage.spec.ts
@@ -0,0 +1,40 @@
+import { createStorage, IStorage } from '../../src/utils/storage';
+import { generateRandomString } from '../test-helpers/generateRandomString';
+
+interface IStoredObject {
+  readonly value: string;
+}
+
+describe('storage.ts', () => {
+  describe('createStorage', () => {
+    let storage: IStorage<IStoredObject>;
+
+    beforeEach(() => {
+      const key = generateRandomString();
+      storage = createStorage<IStoredObject>(key);
+    });
+
+    afterEach(() => {
+      storage.remove();
+    });
+
+    it('should save object to a storage and get it', () => {
+      const testData = { value: 'set-get-test' };
+      storage.set(testData);
+      expect(storage.get()).toEqual({ value: 'set-get-test' });
+
+      const updatedTestData = { value: 'set-get-test-updated' };
+      storage.set(updatedTestData);
+      expect(storage.get()).toEqual({ value: 'set-get-test-updated' });
+    });
+
+    it('should save object to a storage and remove it', () => {
+      const testData = { value: 'set-remove-test' };
+      storage.set(testData);
+      expect(storage.get()).toEqual({ value: 'set-remove-test' });
+
+      storage.remove();
+      expect(storage.get()).toBe(null);
+    });
+  });
+});
