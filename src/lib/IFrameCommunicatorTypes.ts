3ff913bc97be804fbfa583c016d7dad06df081e5
Jan Barton <JanB@kentico.com>
2021-03-09 14:49:08 +0100

    KCL-6794 - SDK initial message with response core + dummy handler

---
+++ src/lib/IFrameCommunicatorTypes.ts
@@ -0,0 +1,37 @@
+export interface IPluginInitializedMessageData {
+  readonly projectId: string | null;
+  readonly languageCodename: string | null;
+  readonly enabled: boolean;
+}
+
+export interface IElementClickedMessageMetadata {
+  readonly elementRect: DOMRect;
+}
+
+export interface IElementClickedMessageData {
+  readonly projectId: string;
+  readonly languageCodename: string;
+  readonly itemId: string;
+  readonly elementCodename: string;
+  readonly contentComponentId: string;
+}
+
+export interface IPluginStatusMessageData {
+  readonly enabled: boolean;
+}
+
+export enum IFrameMessageType {
+  ElementDummy = 'kontent-smart-link:element:dummy',
+  ElementClicked = 'kontent-smart-link:element:clicked',
+  Initialized = 'kontent-smart-link:initialized',
+  Status = 'kontent-smart-link:status',
+}
+
+export enum IFrameResponseType {
+  ElementDummyResponse = 'element-dummy-response',
+}
+
+export interface IFrameResponseMessage {
+  readonly type?: IFrameResponseType;
+  readonly operationId: string;
+}
