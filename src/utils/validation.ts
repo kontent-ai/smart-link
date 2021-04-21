5f148a0a1b03ef7495965ff9dd665c7e797e2e48
Jan Barton <JanB@kentico.com>
2021-04-21 14:29:10 +0200

    KCL-6945 - Edit linked item action

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,10 +1,16 @@
-import { IElementClickedMessageData } from '../lib/IFrameCommunicatorTypes';
+import { IContentItemClickedMessageData, IElementClickedMessageData } from '../lib/IFrameCommunicatorTypes';
 
-export function validateElementClickMessageData(
-  data: Partial<IElementClickedMessageData>
-): data is IElementClickedMessageData {
-  const errors: string[] = [];
+const errors: string[] = [];
 
+const displayErrorsInConsole = (): void => {
+  errors.forEach((error: string) => {
+    console.error(error);
+  });
+};
+
+export function validateEditMessageData(
+  data: Partial<IContentItemClickedMessageData>
+): data is IContentItemClickedMessageData {
   if (!data.projectId) {
     errors.push('Warning: project ID is required to handle element click.');
   }
@@ -17,13 +23,21 @@ export function validateElementClickMessageData(
     errors.push('Warning: item ID is required to handle element click.');
   }
 
+  displayErrorsInConsole();
+
+  return errors.length === 0;
+}
+
+export function validateElementClickMessageData(
+  data: Partial<IElementClickedMessageData>
+): data is IElementClickedMessageData {
+  validateEditMessageData(data);
+
   if (!data.elementCodename) {
     errors.push('Warning: element codename is required to handle element click.');
   }
 
-  errors.forEach((error: string) => {
-    console.error(error);
-  });
+  displayErrorsInConsole();
 
   return errors.length === 0;
 }
