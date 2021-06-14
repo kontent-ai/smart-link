00ebec0ea140822f1e165465e862962fc147c659
Vladislav Bulyukhin <VladislavB@kentico.com>
2021-06-14 12:24:53 +0200

    KCL-6896 Customization options for web components

---
+++ src/web-components/tokens/colors.ts
@@ -0,0 +1,13 @@
+export enum Colors {
+  BackgroundDefault = 'rgba(255, 255, 255, 1)',
+  BackgroundDefaultDisabled = 'rgba(223, 223, 223, 1)',
+  BackgroundDefaultHover = 'rgba(21, 21, 21, 0.1)',
+  BackgroundDefaultSelected = 'rgba(255, 240, 239, 1)',
+  BackgroundSecondary = 'rgba(20, 22, 25, 1)',
+  Primary = 'rgba(219, 60, 0, 1)',
+  PrimaryHover = 'rgba(149, 48, 0, 1)',
+  PrimaryTransparent = 'rgba(219, 60, 0, 0.5)',
+  TextDefault = 'rgba(21, 21, 21, 1)',
+  TextDefaultDisabled = 'rgba(140, 140, 140, 1)',
+  TextSecondary = 'rgba(255, 255, 255, 1)',
+}
---
+++ src/web-components/tokens/shadows.ts
@@ -0,0 +1,4 @@
+export enum Shadows {
+  Default = '0 8px 32px rgba(16, 33, 60, 0.24), 0 0 8px rgba(0, 0, 0, 0.03)',
+  Primary = '0 8px 10px rgba(219, 60, 0, 0.2), 0 6px 20px rgba(219, 60, 0, 0.12), 0 8px 14px rgba(219, 60, 0, 0.14)',
+}
