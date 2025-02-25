1da9f0b8b79d42ee8ce98c94bdc81b59b736a1ae
Ivan Kiral <kiralivan@gmail.com>
2025-02-25 14:36:26 +0100

    remove karma jasmine webpack and storybook

--- .storybook/constants.ts
+++
@@ -1,9 +0,0 @@
-export const DefaultSdkConfigurationWithWs = {
-  queryParam: 'kontent-smart-link-enabled',
-  forceWebSpotlightMode: true,
-};
-
-export const DefaultSdkConfigurationWithoutWs = {
-  queryParam: 'kontent-smart-link-enabled',
-  forceWebSpotlightMode: false,
-};
--- .storybook/main.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  'stories': ['./stories/**/*.stories.mdx', './stories/**/*.stories.@(js|jsx|ts|tsx)'],
-  'addons': ['@storybook/addon-essentials', '@storybook/addon-links', '@storybook/addon-queryparams'],
-  core: {
-    builder: 'webpack5'
-  }
-};
