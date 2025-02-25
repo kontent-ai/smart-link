d700dc34279b39d9dc03b38dc1a774698fba47e4
Ivan Kiral <kiralivan@gmail.com>
2025-02-25 12:42:30 +0100

    update playwright config

--- playwright-ct.config.ts
+++ playwright-ct.config.ts
@@ -5,29 +5,28 @@ import { defineConfig, devices } from '@playwright/experimental-ct-react';
  */
 export default defineConfig({
   testDir: './test-components/tests',
-  /* The base directory, relative to the config file, for snapshot files created with toMatchSnapshot and toHaveScreenshot. */
   snapshotDir: './test-components/__snapshots__',
-  /* Maximum time one test can run for. */
   timeout: 60 * 1000,
+  expect: {
+    timeout: 10000,
+    toHaveScreenshot: {
+      animations: 'disabled',
+      scale: 'css',
+      maxDiffPixels: 20,
+      threshold: 0.2,
+    },
+  },
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
+  retries: 0,
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    /* Port to use for Playwright component endpoint. */
     ctPort: 3100,
-    launchOptions: {
-      ignoreDefaultArgs: ['--hide-scrollbars'],
-    },
   },
 
   /* Configure projects for major browsers */
@@ -36,6 +35,7 @@ export default defineConfig({
       name: 'chromium',
       use: {
         ...devices['Desktop Chrome'],
+        viewport: { width: 1366, height: 768 },
       },
     },
     // {
