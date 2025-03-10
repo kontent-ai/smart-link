90b15730828d4d24f18d669a9be62244dd5faa95
Ivan Kiral <kiralivan@gmail.com>
2025-03-10 09:28:01 +0100

    remove karma from eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,6 @@ module.exports = {
     sourceType: 'module',
   },
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier', 'plugin:prettier/recommended'],
-  ignorePatterns: ['karma.conf.js'],
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/explicit-module-boundary-types': [
