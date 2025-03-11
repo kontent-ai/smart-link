f8bbc9850e6ed41fb9aee39f1d54c405ba4e11ad
Ivan Kiral <kiralivan@gmail.com>
2025-03-11 13:00:07 +0100

    export useful types to public world

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export type { IUpdateMessageData, IRefreshMessageData, IRefreshMessageMetadata } from './lib/IFrameCommunicatorTypes';
 export { default, default as KontentSmartLink, KontentSmartLinkEvent } from './sdk';
 export { buildKontentLink, buildElementLink, buildComponentElementLink } from './utils/link';
 export { applyUpdateOnItemAndLoadLinkedItems, applyUpdateOnItem } from './utils/liveReload';
